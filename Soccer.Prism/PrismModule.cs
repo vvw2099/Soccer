e62f76cca98c6a5d64d80aad71aae2b08ed2f8e8
vvw2099 <vvw2099@hotmail.com>
2020-06-30 18:14:11 -0600

    Project architecture

---
+++ Soccer.Prism/PrismModule.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Soccer.Prism.Views;
+using Soccer.Prism.ViewModels;
+
+namespace Soccer.Prism
+{
+    public class PrismModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<ViewA, ViewAViewModel>();
+        }
+    }
+}
